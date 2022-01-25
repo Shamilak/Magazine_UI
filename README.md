9dab615693ec084f0ba96f599c43845d4523663c
shamilak <shamilabdulazeez9@gmail.com>
2022-01-25 16:33:10 +0530

    work done

---
+++ README.md
@@ -0,0 +1,107 @@
+# Flutter Magazine
+
+Online Magazine store design made in Flutter and inspired by Dribble shot by Daniel Moss. [Design Inspiration](https://dribbble.com/shots/6654840-Magazine-Store)
+
+## Screenshots
+
+<table>
+  <thead>
+    <tr><th align="center">                    Home Screen                </th><th align="center">                    Detail Screen                 </th>  <th align="center">                    About Screen                 </th></tr>
+  </thead>
+  <tbody>
+    <tr><td align="center"> <img src="assets/images/results/screenshot02.png" alt="Home Screen" class="absent" /> </td><td align="center"> <img src="assets/images/results/screenshot01.png" alt="Detail Screen" class="absent" /> </td><td align="center"> <img src="assets/images/results/01.gif" alt="Detail Screen  " class="absent" /> </td></tr>
+  </tbody>
+</table>
+
+## Installation
+
+**Step 1:**
+
+Download or clone this repo by using the link below:
+
+```bash
+https://github.com/Shamilak/Magazine_UI.git
+```
+
+**Step 2:**
+
+Go to project root and execute the following command in console to get the required dependencies: 
+
+```bash
+flutter pub get 
+```
+
+**Step 3:**
+
+Attach android or iOS device and execute following command: 
+
+```bash
+flutter run 
+```
+
+## Project Structure
+
+```
+FlutterMagazine/
+|- android
+|- assets/poster
+|- build
+|- ios
+|- lib
+   |- screens
+   |- widgets
+|- test
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+##  Contribution
+
+**Step 1:**
+
+Fork Repository
+
+```bash
+Click on the Fork
+```
+
+**Step 2:**
+
+Clone Forked repository Repository
+
+```bash
+git clone [Forked Repo link]
+```
+
+**Step 3:**
+
+Make Necessary Changes
+
+**Step 4:**
+
+Syncing a Forked Repository with Upstream
+
+```bash
+https://github.com/Shamilak/Magazine_UI.git
+git fetch upstream
+git checkout master
+git merge upstream/master
+```
+
+**Step 5:**
+
+Push Changes to Forked Repository
+
+```bash
+git push
+```
+
+**Step 6:**
+
+Create Merge Request
+
+```bash
+By navigating to Create New Merge request
+```
+
---
+++ lib/data.dart
@@ -0,0 +1,113 @@
+List data = [
+  {
+    "publishedDate": "20 Nov 2017",
+    "poster": "assets/images/posters/poster1.gif",
+    "price": 8.99,
+    "title": "The New Yorker",
+    "rating": 4.4,
+  },
+  {
+    "publishedDate": "19 Sept 2016",
+    "poster": "assets/images/posters/poster2.gif",
+    "price": 8.99,
+    "title": "The New Yorker",
+    "rating": 4.2,
+  },
+  {
+    "publishedDate": "6 June 2016",
+    "poster": "assets/images/posters/poster3.gif",
+    "price": 8.99,
+    "title": "The New Yorker",
+    "rating": 4.5,
+  },
+  {
+    "publishedDate": "14 Jan 2019",
+    "poster": "assets/images/posters/poster4.gif",
+    "price": 8.99,
+    "title": "The New Yorker",
+    "rating": 4.7,
+  },
+  {
+    "publishedDate": "18 Mar 2019",
+    "poster": "assets/images/posters/poster7.gif",
+    "price": 8.99,
+    "title": "The New Yorker",
+    "rating": 3.9,
+  },
+  {
+    "publishedDate": "22 July 2019",
+    "poster": "assets/images/posters/poster6.gif",
+    "price": 8.99,
+    "title": "The New Yorker",
+    "rating": 4.0,
+  },
+];
+
+List popular = [
+  {
+    "publishedDate": "24 Oct 2016",
+    "poster": "assets/images/posters/poster5.gif",
+    "price": 8.99,
+    "title": "The New Yorker",
+    "rating": 4.9,
+  },
+  {
+    "publishedDate": "19 Sept 2016",
+    "poster": "assets/images/posters/poster8.gif",
+    "price": 8.99,
+    "title": "The New Yorker",
+    "rating": 4.2,
+  },
+  {
+    "publishedDate": "6 June 2016",
+    "poster": "assets/images/posters/poster9.gif",
+    "price": 8.99,
+    "title": "The New Yorker",
+    "rating": 4.5,
+  },
+  {
+    "publishedDate": "14 Jan 2019",
+    "poster": "assets/images/posters/poster10.gif",
+    "price": 8.99,
+    "title": "The New Yorker",
+    "rating": 4.7,
+  },
+];
+
+List newest = [
+  {
+    "publishedDate": "14 Jan 2019",
+    "poster": "assets/images/posters/poster15.gif",
+    "price": 8.99,
+    "title": "The New Yorker",
+    "rating": 4.7,
+  },
+  {
+    "publishedDate": "20 Nov 2017",
+    "poster": "assets/images/posters/poster11.gif",
+    "price": 8.99,
+    "title": "The New Yorker",
+    "rating": 4.4,
+  },
+  {
+    "publishedDate": "19 Sept 2016",
+    "poster": "assets/images/posters/poster12.gif",
+    "price": 8.99,
+    "title": "The New Yorker",
+    "rating": 4.2,
+  },
+  {
+    "publishedDate": "6 June 2016",
+    "poster": "assets/images/posters/poster13.gif",
+    "price": 8.99,
+    "title": "The New Yorker",
+    "rating": 4.5,
+  },
+  {
+    "publishedDate": "14 Jan 2019",
+    "poster": "assets/images/posters/poster14.gif",
+    "price": 8.99,
+    "title": "The New Yorker",
+    "rating": 4.7,
+  },
+];
